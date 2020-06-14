743b4651fc51c21c194c27e30968d46a45841632
Jean Charle Yaacoub <50300488+jyaacoub@users.noreply.github.com>
2020-06-13 21:28:33 -0400

    Cleaning up code and final touches

--- auto-Blinds.ino
+++ auto-Blinds.ino
@@ -6,11 +6,7 @@
 const int lightSunny = 400;
 const int lightDawn = 215;
 const int lightMorning = 215;
-const int lightNight = 30; // to stop actual lights from keeping it open
-// Both lamp and main lights are on => 36
-// only lamp facing it => ~30
-// only main light => ~20
-// None or lamp facing away => ~8
+const int lightNight = 30; // Not 0, to stop room lights from keeping it open
  
 // Number of steps per internal motor revolution
 const float STEPS_PER_REV = 32;
@@ -26,17 +22,10 @@ const float STEPS_PER_OUT_REV = STEPS_PER_REV * GEAR_RED;
 // Number of Steps Required
 int StepsRequired;
  
-// Create Instance of Stepper Class
-// Specify Pins used for motor coils
-// The pins used are 8,9,10,11
-// Connected to ULN2003 Motor Driver In1, In2, In3, In4
-// Pins entered in sequence 1-3-2-4 for proper step sequencing
- 
 Stepper steppermotor(STEPS_PER_REV, 8, 10, 9, 11);
 
 int blindsPos = 0;
 int checks = 0;
-int iterationsPassed = 0;
 const int CLOSED = 12; // The netSteps value for the blinds to be closed.
  
 void setup(){
@@ -52,29 +41,24 @@ void loop(){
   Serial.println(analogRead(LIGHT_SENSOR));
   delay(100);
   userMode();
-  iterationsPassed += 1;
 }
 
 void userMode(){
-  if (iterationsPassed > 10 && checks != 0){
-    checks = 0;
-    iterationsPassed = 0;
-  }
-  
-  int light = getAverageLightIntensity(10); // Calculated over 5 mins (300s)
+  int light = getAverageLightIntensity(10); // Calculated every 10 seconds
   Serial.println(light);
   if (light < 0){
     Serial.println("ERROR");
   }else if ((light > lightSunny) || (light < lightNight)){
-    if (checks >= 3){
+    if (checks >= 12){
       // Closing blinds:
       tiltBlinds(CLOSED); // check isnt reset to zero so it doesnt go up and down due to clouds
+      
     } else {
-      checks += 1;
+      checks += 3;
     }
    
   }else{ // Mornings and dawns
-    if (checks <= -3){
+    if (checks <= -4){
       // Opening blinds:
       tiltBlinds(0);
       checks = 0;
