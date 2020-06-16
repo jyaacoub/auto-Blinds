9b2ab43ce8945b29750ea31ece2ccce189a8381a
jyaacoub <50300488+jyaacoub@users.noreply.github.com>
2020-06-15 23:11:48 -0400

    Update auto-Blinds.ino

--- auto-Blinds.ino
+++ auto-Blinds.ino
@@ -1,58 +1,75 @@
 //Include the Arduino Stepper Library
 #include <Stepper.h>
 #define LIGHT_SENSOR A0
- 
-// Define Constants
+
+// Constants for the motor:
+// Number of steps per internal motor revolution
+const float STEPS_PER_REV = 32;
+//  Amount of Gear Reduction
+const float GEAR_RED = 64;
+// Number of steps per geared output rotation
+const float STEPS_PER_OUT_REV = STEPS_PER_REV * GEAR_RED;
+// Number of Steps Required
+int StepsRequired;
+Stepper steppermotor(STEPS_PER_REV, 8, 10, 9, 11);
+
+// Positioning constants
+int blindsPos = 0;
+int checks = 0;
+const int CLOSED = 12; // The netSteps value for the blinds to be closed.
+
+// Threshold constants:
 const int lightSunny = 400;
 const int lightDawn = 215;
 const int lightMorning = 215;
 const int lightNight = 30; // Not 0, to stop room lights from keeping it open
- 
-// Number of steps per internal motor revolution
-const float STEPS_PER_REV = 32;
- 
-//  Amount of Gear Reduction
-const float GEAR_RED = 64;
- 
-// Number of steps per geared output rotation
-const float STEPS_PER_OUT_REV = STEPS_PER_REV * GEAR_RED;
- 
-// Define Variables
- 
-// Number of Steps Required
-int StepsRequired;
- 
-Stepper steppermotor(STEPS_PER_REV, 8, 10, 9, 11);
 
-int blindsPos = 0;
-int checks = 0;
-const int CLOSED = 12; // The netSteps value for the blinds to be closed.
- 
+// IR stuff
+#include <IRremote.h>  //including infrared remote header file     
+int RECV_PIN = 7; // the pin where you connect the output pin of IR sensor     
+IRrecv irrecv(RECV_PIN);     
+decode_results results;
+
+const int auto_manual_switch;
+const int numbers;
 void setup(){
   Serial.begin(9600);
   // turning off onboard light
   pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
+  digitalWrite(13, LOW);   
   
+  irrecv.enableIRIn();
 //tiltBlinds(-CLOSED);
 }
  
 void loop(){
+  if (irrecv.decode(&results)){ // Returns 0 if no data ready, 1 if data ready   
+   int value = results.value; // Results of decoding are stored in value     
+   Serial.println(" ");     
+   Serial.print("Code: ");     
+   Serial.println(value); //prints the value a a button press     
+   Serial.println(" ");     
+   irrecv.resume(); // Restart the ISR state machine and Receive the next value     
+  } 
+  
   Serial.println(analogRead(LIGHT_SENSOR));
   delay(100);
-  userMode();
+//  autoMode();
 }
 
-void userMode(){
+void manualMode(){
+  
+}
+
+// Leaves the blinds to be controlled by the light intensity
+void autoMode(){
   int light = getAverageLightIntensity(10); // Calculated every 10 seconds
   Serial.println(light);
-  if (light < 0){
-    Serial.println("ERROR");
-  }else if ((light > lightSunny) || (light < lightNight)){
+  if ((light > lightSunny) || (light < lightNight)){
     if (checks >= 12){
       // Closing blinds:
-      tiltBlinds(CLOSED); // check isnt reset to zero so it doesnt go up and down due to clouds
-      
+      tiltBlinds(CLOSED); 
+      // We dont reset check so that it is more resistant against clouds
     } else {
       checks += 3;
     }
@@ -68,12 +85,13 @@ void userMode(){
   }
 }
 
+// timePeriod units = seconds
 int getAverageLightIntensity(int timePeriod){
-  // time period is num of seconds to calculate average over
   int total = 0;
   for (int i = 0; i < timePeriod; i++){
     int light = analogRead(LIGHT_SENSOR);
     if (light < 0){
+      Serial.print("ERROR: ");
       Serial.println(light);
     }
     total += light;
@@ -83,6 +101,7 @@ int getAverageLightIntensity(int timePeriod){
   return (total/timePeriod);
 }
 
+// pos 0 == blinds are horizontal
 void tiltBlinds(int pos){
   // The dif is also equal to the num steps to take.
   int dif = pos - blindsPos;
@@ -98,17 +117,18 @@ void tiltBlinds(int pos){
   }
 }
 
+// directionIn: 
+//    0 = counter-clockwise 
+//    1 = clockwise
 void turnMotor(int directionIn, float numSteps){
-  if (directionIn == 0){
-//    Serial.println("Turing counter clockwise..."); // UP
+  if (directionIn == 0){  // Turns it counter-clockwise to tilt blinds UP
     StepsRequired  = -(STEPS_PER_OUT_REV/ 2)* numSteps;
     steppermotor.setSpeed(700);  
     steppermotor.step(StepsRequired);
 
     blindsPos += numSteps;
    
-  } else{
-//    Serial.println("Turing clockwise..."); //DOWN
+  } else{ // Turns it clockwise to tilt blinds DOWN
     StepsRequired  = (STEPS_PER_OUT_REV/ 2)* numSteps;
     steppermotor.setSpeed(700);  
     steppermotor.step(StepsRequired);
@@ -116,6 +136,8 @@ void turnMotor(int directionIn, float numSteps){
     blindsPos -= numSteps;
   }
 }
+
+// Extra Functions for debugging
 
 void stepDebugger(){
   Serial.println("\t NET STEPS: " + String(blindsPos));
