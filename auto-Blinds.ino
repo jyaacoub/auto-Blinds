f7fe1a7e21d4bdd8664b62f6a2e87a8f5ae816bf
jyaacoub <50300488+jyaacoub@users.noreply.github.com>
2020-06-15 23:41:25 -0400

    Update auto-Blinds.ino

--- auto-Blinds.ino
+++ auto-Blinds.ino
@@ -46,80 +46,56 @@ void setup(){
 void loop(){
   if (irrecv.decode(&results)){
       Serial.println(results.value);
-//      if (results.value == 0XFFFFFFFF)
-//        results.value = key_value;
-//
-//      switch(results.value){
-//        case 0xFFA25D:
-//        Serial.println("CH-");
-//        break;
-//        case 0xFF629D:
-//        Serial.println("CH");
-//        break;
-//        case 0xFFE21D:
-//        Serial.println("CH+");
-//        break;
-//        case 0xFF22DD:
-//        Serial.println("|<<");
-//        break;
-//        case 0xFF02FD:
-//        Serial.println(">>|");
-//        break ;  
-//        case 0xFFC23D:
-//        Serial.println(">|");
-//        break ;               
-//        case 0xFFE01F:
-//        Serial.println("-");
-//        break ;  
-//        case 0xFFA857:
-//        Serial.println("+");
-//        break ;  
-//        case 0xFF906F:
-//        Serial.println("EQ");
-//        break ;  
-//        case 0xFF6897:
-//        Serial.println("0");
-//        break ;  
-//        case 0xFF9867:
-//        Serial.println("100+");
-//        break ;
-//        case 0xFFB04F:
-//        Serial.println("200+");
-//        break ;
-//        case 0xFF30CF:
-//        Serial.println("1");
-//        break ;
-//        case 0xFF18E7:
-//        Serial.println("2");
-//        break ;
-//        case 0xFF7A85:
-//        Serial.println("3");
-//        break ;
-//        case 0xFF10EF:
-//        Serial.println("4");
-//        break ;
-//        case 0xFF38C7:
-//        Serial.println("5");
-//        break ;
-//        case 0xFF5AA5:
-//        Serial.println("6");
-//        break ;
-//        case 0xFF42BD:
-//        Serial.println("7");
-//        break ;
-//        case 0xFF4AB5:
-//        Serial.println("8");
-//        break ;
-//        case 0xFF52AD:
-//        Serial.println("9");
-//        break ;      
-//      }
-//      key_value = results.value;
-//      irrecv.resume(); 
+      if (results.value == 0XFFFFFFFF)
+        results.value = key_value;
+
+      switch(results.value){
+        case 0xFFA25D:
+          Serial.println("ON");
+          break;
+        case 0xFF629D:
+          Serial.println("TIMER");
+          break;
+        case 0xFFE21D:
+          Serial.println("OFF");
+          break;
+        case 0xFF22DD:
+          Serial.println("1");
+          break;
+        case 0xFFC23D:
+          Serial.println("2");
+          break ;               
+        case 0xFFE01F:
+          Serial.println("3");
+          break ;  
+        case 0xFF906F:
+          Serial.println("4");
+          break ;  
+        case 0xFF6897:
+          Serial.println("5");
+          break ;
+        case 0xFFB04F:
+          Serial.println("6");
+          break ;
+        case 0xFF30CF:
+          Serial.println("7");
+          break ;
+        case 0xFF7A85:
+          Serial.println("8");
+          break ;
+        case 0xFF10EF:
+          Serial.println("-Bright");
+          break ;
+        case 0xFF5AA5:
+          Serial.println("+Bright");
+          break ;      
+      }
+      key_value = results.value;
+      irrecv.resume(); 
   }
   
-  Serial.println(analogRead(LIGHT_SENSOR));
-  delay(100);
+//  Serial.println(analogRead(LIGHT_SENSOR));
+//  delay(100);
 //  autoMode();
 }
 
