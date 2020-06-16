ff1caa872d7366d0ae4812df959aa1000fdabedb
jyaacoub <50300488+jyaacoub@users.noreply.github.com>
2020-06-16 12:01:14 -0400

    Update auto-Blinds.ino

--- auto-Blinds.ino
+++ auto-Blinds.ino
@@ -95,9 +95,11 @@ void readIRSignal(){
           break ;
         case 0xFF30CF:
           Serial.println("7");
+          newPos = 0; // opens
           break ;
         case 0xFF7A85:
           Serial.println("8");
+          newPos = CLOSED;
           break ;
         case 0xFF10EF:
           Serial.println("-Bright");
